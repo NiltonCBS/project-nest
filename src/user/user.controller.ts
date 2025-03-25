3498546f03958f4579ee76c12a5ef2ad1b8fdfee
Nilton Cesar Barbosa dos Santos <nilton.santos01@fatec.sp.gov.br>
2025-03-25 14:57:51 -0300

    CRUD finalizado

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -11,4 +11,25 @@ export class UserController {
     async create(@Body() data:UserDto){
         return await this.userService.create(data);
     }
+
+    @Get()
+    async findAll(){
+        return await this.userService.findAll();
+    }
+
+    @Put(":id")
+    async update(@Param("id") id: number, @Body() data: UserDto){
+        return this.userService.update(Number(id), data)
+    }
+
+    @Delete(":id")
+    async delete(@Param("id") id: number){
+        return this.userService.delete(Number(id))
+    }
+
+    @Get(":id")
+    async findById(@Param("id") id: number){
+        return this.userService.findById(Number(id))
+    }
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,4 +13,49 @@ export class UserService {
 
         return user;
     }
+
+    async findAll(){
+        return await this.prisma.user.findMany();
+    }
+
+    async update(id: number, data: UserDto) {
+        const userExists = await this.prisma.user.findUnique({
+            where: {id},
+        });
+
+        if(!userExists){
+            throw new Error('User não encontrado');
+        }
+
+        return await this.prisma.user.update({
+            data,
+            where: {id},
+        })
+    }
+
+    async delete(id:number) {
+        const userExists = await this.prisma.user.findUnique({
+            where: {id},
+        });
+
+        if(!userExists){
+            throw new Error('User não encontrado');
+        }
+
+        return await this.prisma.user.delete({
+            where: {id},
+        });
+    }
+
+    async findById(id: number){
+        const userExists = await this.prisma.user.findUnique({
+            where: {id},
+        });
+
+        if(!userExists){
+            throw new Error('User não encontrado');
+        }
+
+        return userExists;
+    }
 }
